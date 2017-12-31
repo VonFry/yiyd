3af0cac636efbb60d38cb5ed20999a4b7b15e55e
Vonfry <vonfry314@gmail.com>
2017-12-31 11:40:09 +0800

    feat(divine): convert gua
    add todo & convert gua to other format

--- src/ZY/Divination.hs
+++ src/ZY/Divination.hs
@@ -4,6 +4,8 @@ module ZY.Divination where
 
 import Arguments (Arguments)
 
+type Gua = [Int]
+
 divine
     :: Arguments -- ^ arguments in the cli
     -> String -- ^ data for string
@@ -14,3 +16,26 @@ divine args dataStr = Right "error"
 generateYao
     :: Int -- ^ return 9, 8, 7, 6.
 generateYao = -1
+
+-- | convert to 本卦.
+convertToGua
+    :: Gua -- ^ origin six Yao number
+    -> Gua -- ^ convert six Yao number
+convertToGua = map (\y -> if y `mod` 2 == 1 then 9 else 6)
+
+-- | convert to 之卦
+convertToZhiGua
+    :: Gua -- ^ origin six Yao number
+    -> Gua -- ^ convert six Yao number
+convertToZhiGua = convertToGua . map (\y ->
+    case y of
+         9 -> 6
+         6 -> 9
+         _ -> y)
+
+-- | read gua from zhouyi
+readGuaNumber
+    :: Gua    -- ^ converted gua
+    -> String -- ^ data for string
+    -> Int    -- ^ number of the gua
+readGuaNumber g d = -1
